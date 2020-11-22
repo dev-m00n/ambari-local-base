56f08a433cbf51de93e7a7c6d22fc4bb297182b8
Moon <munduk.hyun@agoda.com>
2020-11-22 15:06:52 +0700

    Fix huge size of image if the UID is big

--- scripts/add_admin.sh
+++ scripts/add_admin.sh
@@ -11,7 +11,7 @@ if [ "$AMBARI_UID" != "" ]; then
   NEW_UID=$AMBARI_UID
 fi
 
-adduser $NEW_USER_NAME -u $NEW_UID -g 0
+adduser $NEW_USER_NAME -u $NEW_UID -g 0 --no-log-init #--no-log-init is helpful if the uid is big(https://stackoverflow.com/questions/48671214/docker-image-size-for-different-user)
 passwd -d $NEW_USER_NAME
 
 echo "Ambari user: $NEW_USER_NAME: $NEW_UID has been create"
